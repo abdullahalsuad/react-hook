7405e5aa2ce44cfee89b1f391980981ab3ccaa1c
Harun-Ar- Rashid <suadabdullahal@gmail.com>
2021-07-18 17:45:58 +0600

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import { useState } from 'react';
 import Todo from './component/Todo'
 import Counter from './component/Counter'
+import MyComponent from './component/MyComponent'
 
-export default class App extends React.Component {
-  render() {
+
+export default function App () {
+  const [show, setShow] = useState(true);
+
     return (
       <div>
         <Todo/>
         <Counter/>
+        <div>{show && <MyComponent />}</div>
+            <p>
+                <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
+                    {show ? 'Hide post' : 'Show post'}
+                </button>
+            </p>
       </div>
     )
-  }
 }
 
 
--- src/component/Counter.js
+++ src/component/Counter.js
@@ -4,8 +4,8 @@ export default function Counter() {
     const [count, setCount] = useState(0);
     let i = 0;
     const addFive = () => {
-        while (i < 5){
-            setCount((prevState) => prevState + 1 )
+        while (i < 1){
+            setCount((prevState) => prevState - 1 )
             i += 1;
         }
     }
@@ -14,11 +14,11 @@ export default function Counter() {
             {count}
             <p>
                 <button type="button" onClick={() => setCount((prevState) => prevState + 1)}>
-                    Add 1 
+                    Add +1 
                 </button>
                 <br></br>
                 <button type="button" onClick={addFive}>
-                    Add 5
+                    Add -1
                 </button>
             </p>
         </div>
