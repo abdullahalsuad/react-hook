92dfd4f90edfd07d8136700830833a4b3366ee62
Harun-Ar- Rashid <suadabdullahal@gmail.com>
2021-07-18 15:10:43 +0600

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Todo from './component/Todo'
+import Counter from './component/Counter'
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <Todo/>
+        <Counter/>
       </div>
     )
   }
