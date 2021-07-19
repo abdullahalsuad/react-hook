6537e24fef05dc8ee84b611b602efbc751042d60
Harun-Ar- Rashid <suadabdullahal@gmail.com>
2021-07-19 16:22:48 +0600

    useMemo

--- src/App.js
+++ src/App.js
@@ -23,19 +23,12 @@ export default function App () {
     setCount2((prevCount) => prevCount + 5);
   },[])
 
-  // const incrementByOne = useCallback(() => {
-  //     setCount1((prevCount) => prevCount + 1);
-  // }, []);
-
-  // const incrementByFive = useCallback(() => {
-  //     setCount2((prevCount) => prevCount + 5);
-  // }, []);
-
-  // const isEvenOrOdd = useMemo(() => {
-  //     let i = 0;
-  //     while (i < 1000000000) i += 1; // costly operation
-  //     return count1 % 2 === 0;
-  // }, [count1]);
+   
+  const isEvenOrOdd = useMemo(() => {
+      let i = 0;
+      while (i < 1000000000) i += 1; // costly operation
+      return count1 % 2 === 0;
+  }, [count1]);
 
 
     return (
@@ -52,7 +45,7 @@ export default function App () {
             <div >
             <Title />
             <ShowCount count={count1} title="Counter 1" />
-            {/* <span>{isEvenOrOdd ? 'Even' : 'Odd'}</span> */}
+            <span>{isEvenOrOdd ? 'Even' : 'Odd'}</span>
             <Button handleClick={incrementByOne}>Increment by one</Button>
             <hr />
             <ShowCount count={count2} title="Counter 2" />
