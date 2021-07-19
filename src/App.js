7e89eb287609820919d974f1283e8e4132458baf
Harun-Ar- Rashid <suadabdullahal@gmail.com>
2021-07-19 17:11:31 +0600

    useRef

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
 /* eslint-disable no-unused-vars */
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useMemo, useState} from 'react';
 // import Todo from './component/Todo'
 // import Counter from './component/Counter'
 // import MyComponent from './component/MyComponent'
-
-import Button from './component/Button';
-import ShowCount from './component/ShowCount';
-import Title from './component/Title';
-
+// import Button from './component/Button';
+// import ShowCount from './component/ShowCount';
+// import Title from './component/Title';
+import Form from './component/Form';
+import Time from './component/Time';
 
 export default function App () {
   // const [show, setShow] = useState(true);
 
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
+  // const [count1, setCount1] = useState(0);
+  // const [count2, setCount2] = useState(0);
 
-  const incrementByOne = useCallback(() =>{
-    setCount1((prevCount) => prevCount + 1);
-  },[]) 
+  // const incrementByOne = useCallback(() =>{
+  //   setCount1((prevCount) => prevCount + 1);
+  // },[]) 
 
-  const incrementByFive = useCallback(() =>{
-    setCount2((prevCount) => prevCount + 5);
-  },[])
+  // const incrementByFive = useCallback(() =>{
+  //   setCount2((prevCount) => prevCount + 5);
+  // },[])
 
-   
-  const isEvenOrOdd = useMemo(() => {
-      let i = 0;
-      while (i < 1000000000) i += 1; // costly operation
-      return count1 % 2 === 0;
-  }, [count1]);
-
+  // const isEvenOrOdd = useMemo(() => {
+  //     let i = 0;
+  //     while (i < 1000000000) i += 1; // costly operation
+  //     return count1 % 2 === 0;
+  // }, [count1]);
 
     return (
       <div>
-        {/* <Todo/>
+         {/* <Todo/>
         <Counter/>
         <div>{show && <MyComponent />}</div>
             <p>
                 <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
                     {show ? 'Hide post' : 'Show post'}
                 </button>
-            </p> */}
+            </p> 
 
             <div >
             <Title />
@@ -50,8 +48,9 @@ export default function App () {
             <hr />
             <ShowCount count={count2} title="Counter 2" />
             <Button handleClick={incrementByFive}>Increment by five</Button>
-        </div>
-
+        </div> */}
+       <Form /> 
+       <Time/>
       </div>
     )
 }
---
+++ src/component/Form.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useRef } from 'react'
+import Input from './Input';
+
+export default function Form() {
+    
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef.current.focus();
+    },[])
+
+
+    return (
+        <div>
+            <Input ref={inputRef} type="text"  placeholder="enter something" /> 
+        </div>
+    )
+}
